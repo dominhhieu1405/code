1f08a40121bee5437afaa45d2f40e1b920f2ce46
dominhhieu1405 <51281387+dominhhieu1405@users.noreply.github.com>
2019-12-18 19:07:46 +0700

    Create abc.js

---
+++ totinh/js/abc.js
@@ -0,0 +1,38 @@
+function showbg() {
+if (document.f1.giaodien.value == "giaodien0") {
+var x = "<img src='https://dominhhieu1405.github.io/code/totinh/img/type1.PNG' style='display:none'>";
+shows.innerHTML=x;
+}
+if (document.f1.giaodien.value == "giaodien1") {
+var x = "<img src='https://dominhhieu1405.github.io/code/totinh/img/type1.PNG' width='350' height='200' >";
+shows.innerHTML=x;
+}
+
+if (document.f1.giaodien.value == "giaodien2") {
+var x = "<img src='https://dominhhieu1405.github.io/code/totinh/img/type2.PNG' width='350' height='200'>";
+shows.innerHTML=x;
+}
+
+if (document.f1.giaodien.value == "giaodien3") {
+var x = "<img src='https://dominhhieu1405.github.io/code/totinh/img/type3.PNG' width='350' height='200'>";
+shows.innerHTML=x;
+}
+
+if (document.f1.giaodien.value == "giaodien4") {
+var x = "<img src='https://dominhhieu1405.github.io/code/totinh/img/type4.PNG' width='350' height='200'>";
+shows.innerHTML=x;
+}
+
+if (document.f1.giaodien.value == "giaodien5") {
+var x = "<img src='https://dominhhieu1405.github.io/code/totinh/img/type5.PNG' width='350' height='200'>";
+shows.innerHTML=x;
+}
+if (document.f1.giaodien.value == "giaodien6") {
+var x = "<img src='https://dominhhieu1405.github.io/code/totinh/img/type6.PNG' width='350' height='200'>";
+shows.innerHTML=x;
+}
+if (document.f1.giaodien.value == "giaodien7") {
+var x = "<img src='https://dominhhieu1405.github.io/code/totinh/img/type7.PNG' width='350' height='200'>";
+shows.innerHTML=x;
+}
+}
